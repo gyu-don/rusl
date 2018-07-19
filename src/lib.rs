5a05d956f5ae083369610ee3c7b9edf3a18a497c
gyu-don <takumi.kt+git@gmail.com>
2018-07-19 09:19:45 +0900

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+#![no_std]
+
+#[macro_use]
+extern crate syscall;
+pub mod unistd;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn getpid() {
+        use unistd::getpid;
+        assert!(getpid() > 1);
+    }
+}
---
+++ src/unistd/mod.rs
@@ -0,0 +1,4 @@
+mod types;
+mod getpid;
+pub use self::types::*;
+pub use self::getpid::getpid;
